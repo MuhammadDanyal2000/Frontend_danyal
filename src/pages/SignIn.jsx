0438294311d99b0bcecb95d79e2b8d4cc4bb0c2f
MuhammadDanyal2000 <danyalhafeez46@gmail.com>
2023-10-31 23:48:31 +0500

    updated

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 const SignIn = () => {
   return (
     <div className='flex justify-center items-center px-12 py-28'>
-    <div className="flex w-auto py-6 flex-col bg-[#2a2727] bg-opacity-80 justify-center items-center  lg:px-8 ">
+    <div className="flex w-auto py-6 flex-col sm:bg-[#2a2727] sm:bg-opacity-80 items-center  sm:px-8 ">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-orange-500 sm:max-w-sm">
-            Sign in to your account
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white sm:max-w-sm">
+          <span className='text-yellow-500'>Sign In</span> to your account
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-10 px-3 mx-auto w-full max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-orange-500">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-yellow-500">
                 Email address
               </label>
               <div className="mt-2">
@@ -31,7 +31,7 @@ const SignIn = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-orange-500">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-yellow-500">
                   Password
                 </label>
                 
@@ -51,7 +51,7 @@ const SignIn = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 mb-10"
+                className="flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 mb-10"
               >
                 Sign in
               </button>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div className="flex justify-center items-center sm:px-12 sm:py-28">
-    <div className="flex  w-auto py-6 flex-1 flex-col justify-center bg-[#2a2727] bg-opacity-80 items-center lg:px-8 ">
+    <div className="flex justify-center items-center px-12 py-28">
+    <div className="flex  w-auto py-6 flex-col sm:bg-[#2a2727] sm:bg-opacity-80 items-center sm:px-8 ">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-orange-500">
-            Don't have a acccount Sign Up
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white sm:max-w-sm">
+            Don't have a acccount <span className='text-yellow-500'>Sign Up</span>
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-10 px-3 mx-auto w-full max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-orange-500">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-yellow-500">
                 Email address
               </label>
               <div className="mt-2">
@@ -31,7 +31,7 @@ const SignUp = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-orange-500">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-yellow-500">
                   Password
                 </label>
                
@@ -52,7 +52,7 @@ const SignUp = () => {
             
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-orange-500">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-yellow-500">
                  Confirm Password
                 </label>
                
@@ -74,7 +74,7 @@ const SignUp = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full mb-10 justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 mb-10"
+                className="flex w-full mb-10 justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-500 hover:text-purple-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 mb-10"
               >
                 Sign Up
               </button>
